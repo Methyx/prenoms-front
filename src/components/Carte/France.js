94a7f050d591bfd4956d37cd2ed844b746ed1fa0
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2023-01-06 12:36:39 +0100

    Map France interactive following

--- src/components/Carte/France.js
+++ src/components/Carte/France.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { SvgLoader, SvgProxy } from "react-svgmt";
+import { useEffect, useState } from "react";
 
 // functions
 import normalizeDpt from "../../functions/normalizeDpt";
@@ -9,11 +10,31 @@ import Carte from "./Carte";
 
 import "../../style/france.css";
 
-const France = ({ defaultColor, departements, highlightColor }) => {
-  // Init
-  let selected = null;
-  let oldSelectedColor = "";
+const France = ({
+  defaultColor,
+  departements,
+  highlightColor,
+  setDptSelected,
+}) => {
+  // Use States
+  const [dptColor, setDptColor] = useState({});
+  const [isReady, setIsReady] = useState(false);
+  const [oldSelected, setOldSelected] = useState(null);
 
+  //
+  useEffect(() => {
+    const tabColor = {};
+    for (let i = 0; i < listOfDepartements.length; i++) {
+      tabColor[normalizeDpt(listOfDepartements[i])] = defaultColor;
+    }
+    for (let i = 0; i < departements.length; i++) {
+      tabColor[normalizeDpt(departements[i].code)] = departements[i].color;
+    }
+    setDptColor({ ...tabColor });
+    setIsReady(true);
+  }, []);
+
+  //
   return (
     <SvgLoader
       svgXML={Carte}
@@ -24,39 +45,37 @@ const France = ({ defaultColor, departements, highlightColor }) => {
           e.target.id !== "dpt-75-92-93-94"
         ) {
           let unselect = false;
-          if (selected) {
-            selected.attributes.fill.nodeValue = oldSelectedColor;
-            if (selected.id === e.target.id) {
-              selected = null;
-              oldSelectedColor = "";
+          const colors = { ...dptColor };
+          if (oldSelected) {
+            colors[oldSelected.id.slice(4)] = oldSelected.color;
+            if (oldSelected.id === e.target.id) {
+              setOldSelected(null);
+              setDptSelected(null);
               unselect = true;
             }
           }
           if (!unselect) {
-            selected = e.target;
-            oldSelectedColor = selected.attributes.fill.nodeValue;
-            e.target.attributes.fill.nodeValue = highlightColor;
+            setOldSelected({
+              id: e.target.id,
+              color: e.target.attributes.fill.nodeValue,
+            });
+            setDptSelected(e.target.id);
+            colors[e.target.id.slice(4)] = highlightColor;
           }
+          setDptColor(colors);
         }
       }}
     >
-      {listOfDepartements.map((dpt) => {
-        return (
-          <SvgProxy
-            key={`default_${dpt}`}
-            selector={`#dpt-${normalizeDpt(dpt)}`}
-            fill={defaultColor}
-          />
-        );
-      })}
-      {/* <SvgProxy selector="#carte" fill={defaultColor} /> */}
-      {departements.map((dpt) => (
-        <SvgProxy
-          key={`colorized_${dpt.code}`}
-          selector={`#dpt-${normalizeDpt(dpt.code)}`}
-          fill={dpt.color}
-        />
-      ))}
+      {isReady &&
+        listOfDepartements.map((dpt) => {
+          return (
+            <SvgProxy
+              key={`default_${dpt}`}
+              selector={`#dpt-${normalizeDpt(dpt)}`}
+              fill={dptColor[normalizeDpt(dpt)]}
+            />
+          );
+        })}
     </SvgLoader>
   );
 };
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,35 @@
+import { useState } from "react";
+
+// components
 import France from "./Carte/France";
 
 // style
 import "../style/mapTest2.css";
 
 const Map = () => {
+  // UseStates
+  const [dptSelected, setDptSelected] = useState(null);
+
   return (
-    <div className="map-container">
-      <France
-        departements={[
-          { code: 75, color: "red" },
-          { code: 89, color: "blue" },
-          { code: 21, color: "green" },
-          { code: "2a", color: "violet" },
-          { code: 976, color: "salmon" },
-        ]}
-      />
-    </div>
+    <>
+      <div className="map-container">
+        <France
+          departements={[
+            { code: 75, color: "red" },
+            { code: 89, color: "blue" },
+            { code: 21, color: "green" },
+            { code: "2a", color: "violet" },
+            { code: 976, color: "salmon" },
+          ]}
+          setDptSelected={setDptSelected}
+        />
+      </div>
+      {dptSelected ? (
+        <p>Vous avez selectionné : {dptSelected}</p>
+      ) : (
+        <p>Vous pouvez selectionner un département</p>
+      )}
+    </>
   );
 };
 
