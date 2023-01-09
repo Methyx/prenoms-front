64b06bde00c9b7301b894896a18739f2f45377a4
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2023-01-09 18:54:14 +0100

    home page with CSS

--- src/components/TopNames.js
+++ src/components/TopNames.js
@@ -45,34 +45,61 @@ const TopNames = ({ years, gender }) => {
   return (
     <div className="top-names">
       <div className="map-container">
-        <France dptSelected={dptSelected} setDptSelected={setDptSelected} />
+        <France
+          highlightColor={"#ff6e40"}
+          dptSelected={dptSelected}
+          setDptSelected={setDptSelected}
+        />
       </div>
       <div className="results-container">
-        <h1>Classement des Prénoms</h1>
+        <h1>
+          Classement des Prénoms {gender === "M" && "Masculins"}
+          {gender === "F" && "Féminins"} {" - "} {years}
+        </h1>
         {dptSelected ? (
-          <h1>
-            dans le département {dptSelected.slice(4)} :{" "}
-            {getNameOfDpt(dptSelected.slice(4))}{" "}
-          </h1>
+          <h2>
+            {getNameOfDpt(dptSelected.slice(4))}
+            {" ("}
+            {dptSelected.slice(4)}
+            {" )"}
+          </h2>
         ) : (
-          <h1>en FRANCE</h1>
+          <h2>FRANCE entière</h2>
         )}
+        <h6>(Cliquer sur la carte pour sélectionner un département)</h6>
         <div className="chart">
-          {isReady ? (
-            data[0]?._id ? (
-              data.map((item, index) => {
-                return (
-                  <p key={index}>
-                    N° {index + 1} : {item._id} - {item.total}
-                  </p>
-                );
-              })
-            ) : (
-              <p>Pas de données</p>
-            )
-          ) : (
-            <p>Loading ...</p>
-          )}
+          <table>
+            <thead>
+              <tr>
+                <th>Rang</th>
+                <th>Prénom</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {isReady ? (
+                data[0]?._id ? (
+                  data.map((item, index) => {
+                    return (
+                      <tr key={index + 1}>
+                        <td>{index + 1}</td>
+                        <td>{item._id} </td>
+                        <td>{item.total} </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <tr>
+                    <td colSpan={3}>Pas de données</td>
+                  </tr>
+                )
+              ) : (
+                <tr>
+                  <td colSpan={3}>Loading ...</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,70 @@
+import { useState } from "react";
+
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+
 // components
 import TopNames from "../components/TopNames";
 
+// style
+import "../style/home.css";
+
 const Home = () => {
+  // Use States
+  const [years, setYears] = useState("2020");
+  const [year, setYear] = useState("2020");
+  const [gender, setGender] = useState("");
+
   return (
-    <div className="container">
-      <h1>HOME PAGE</h1>
-      <TopNames years={2010} gender={"F"} />
+    <div className="container home-page">
+      <div className="selections">
+        <div>
+          <TextField
+            label="Année"
+            type="number"
+            value={year}
+            onChange={(event) => {
+              setYear(event.target.value);
+              if (
+                Number(event.target.value) >= 1900 &&
+                Number(event.target.value) < 2022
+              ) {
+                setYears(event.target.value);
+              }
+            }}
+          />
+          {years !== year && <p>entrée incorrecte</p>}
+        </div>
+        <FormControl>
+          <RadioGroup
+            row
+            value={gender}
+            onChange={(event) => {
+              setGender(event.target.value);
+            }}
+          >
+            <FormControlLabel
+              value="F"
+              control={<Radio sx={{ "&.Mui-checked": { color: "#ff6e40" } }} />}
+              label="Femme"
+            />
+            <FormControlLabel
+              value="M"
+              control={<Radio sx={{ "&.Mui-checked": { color: "#ff6e40" } }} />}
+              label="Homme"
+            />
+            <FormControlLabel
+              value=""
+              control={<Radio sx={{ "&.Mui-checked": { color: "#ff6e40" } }} />}
+              label="Indifférent"
+            />
+          </RadioGroup>
+        </FormControl>
+      </div>
+      <TopNames years={years} gender={gender} />
     </div>
   );
 };
