304a7a72001d550beb074b91596e9d3e84c62404
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2023-01-10 11:40:55 +0100

    home page OK

--- src/components/TopNames.js
+++ src/components/TopNames.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import TextField from "@mui/material/TextField";
 
 // components
 import France from "./Carte/France";
@@ -15,6 +16,8 @@ const TopNames = ({ years, gender }) => {
   const [isReady, setIsReady] = useState(false);
   const [dptSelected, setDptSelected] = useState(null);
   const [data, setData] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const memoElement = useRef(null);
 
   useEffect(() => {
     const loadData = async () => {
@@ -40,6 +43,8 @@ const TopNames = ({ years, gender }) => {
       }
     };
     loadData(years);
+    setSearchName("");
+    memoElement.current = null;
   }, [dptSelected, gender, years]);
 
   return (
@@ -54,7 +59,7 @@ const TopNames = ({ years, gender }) => {
       <div className="results-container">
         <h1>
           Classement des Prénoms {gender === "M" && "Masculins"}
-          {gender === "F" && "Féminins"} {" - "} {years}
+          {gender === "F" && "Féminins"} {" - "} {years || "all"}
         </h1>
         {dptSelected ? (
           <h2>
@@ -81,10 +86,10 @@ const TopNames = ({ years, gender }) => {
                 data[0]?._id ? (
                   data.map((item, index) => {
                     return (
-                      <tr key={index + 1}>
+                      <tr key={index + 1} id={item._id}>
                         <td>{index + 1}</td>
                         <td>{item._id} </td>
-                        <td>{item.total} </td>
+                        <td>{Number(item.total).toLocaleString()} </td>
                       </tr>
                     );
                   })
@@ -100,6 +105,44 @@ const TopNames = ({ years, gender }) => {
               )}
             </tbody>
           </table>
+        </div>
+        <div className="search-name">
+          <TextField
+            className="input-name"
+            sx={{
+              "& label.Mui-focused": {
+                color: "#ff6e40",
+              },
+              "& .MuiOutlinedInput-root": {
+                "&.Mui-focused fieldset": {
+                  borderColor: "#ff6e40",
+                },
+              },
+            }}
+            type="text"
+            label="Rechercher un prénom"
+            value={searchName}
+            onChange={(event) => {
+              setSearchName(event.target.value);
+              const elem = document.getElementById(
+                event.target.value.toUpperCase()
+              );
+              if (elem) {
+                elem.scrollIntoView();
+                memoElement.current = {
+                  element: elem,
+                  color: window.getComputedStyle(elem).color,
+                };
+                elem.style.color = "#ff6e40";
+              } else {
+                if (memoElement.current) {
+                  memoElement.current.element.style.color =
+                    memoElement.current.color;
+                  memoElement.current = null;
+                }
+              }
+            }}
+          />
         </div>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,28 +15,61 @@ import "../style/home.css";
 const Home = () => {
   // Use States
   const [years, setYears] = useState("2020");
-  const [year, setYear] = useState("2020");
+  const [yearsInput, setYearsInput] = useState("2020");
   const [gender, setGender] = useState("");
 
+  // Functions
+  const handleYearsInput = (yearEntry) => {
+    if (yearEntry === "all") {
+      setYears("");
+    } else {
+      const yearsArray = yearEntry.split("-");
+      if (Number(yearsArray[0]) >= 1900 && Number(yearsArray[0]) < 2022) {
+        if (yearsArray.length === 1) {
+          setYears(yearEntry);
+        } else if (yearsArray.length === 2) {
+          if (
+            Number(yearsArray[1]) >= Number(yearsArray[0]) &&
+            Number(yearsArray[1]) < 2022
+          ) {
+            setYears(yearEntry);
+          }
+        }
+      }
+    }
+  };
+
+  // ==============
   return (
     <div className="container home-page">
+      <div className="title">
+        <h1>Le site des prénoms donnés en France entre 1900 et 2021</h1>
+        <p>Source : fichier des prénoms de l'INSEE</p>
+      </div>
       <div className="selections">
         <div>
           <TextField
-            label="Année"
-            type="number"
-            value={year}
+            label="Année(s)"
+            type="text"
+            sx={{
+              "& label.Mui-focused": {
+                color: "#ff6e40",
+              },
+              "& .MuiOutlinedInput-root": {
+                "&.Mui-focused fieldset": {
+                  borderColor: "#ff6e40",
+                },
+              },
+            }}
+            value={yearsInput}
             onChange={(event) => {
-              setYear(event.target.value);
-              if (
-                Number(event.target.value) >= 1900 &&
-                Number(event.target.value) < 2022
-              ) {
-                setYears(event.target.value);
-              }
+              setYearsInput(event.target.value);
+              handleYearsInput(event.target.value);
             }}
           />
-          {years !== year && <p>entrée incorrecte</p>}
+          {(years !== yearsInput || years === "all") && (
+            <p>ex.: 2015 ou 2010-2020</p>
+          )}
         </div>
         <FormControl>
           <RadioGroup
